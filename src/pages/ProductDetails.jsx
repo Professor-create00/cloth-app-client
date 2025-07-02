549b41d19ce7ba34f999e377cf5341948ea4505d
lakhan sharan <lakhansharan946@gmail.com>
2025-07-02 19:50:15 +0530

    fixed images to image everywhere

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -56,7 +56,7 @@ const ProductDetails = () => {
     <>
       <Navbar />
       <div className="max-w-6xl mx-auto px-4 py-10 grid grid-cols-1 md:grid-cols-2 gap-10">
-        {/* Product images section */}
+       
         <div>
           <img
             src={selectedImage}
